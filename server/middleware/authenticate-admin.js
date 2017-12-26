f1b6ec485b532c7985661500e7aa2eee5af64bee
Michele Biondi <michelebiondi01@gmail.com>
2017-12-26 18:17:40 +0100

    Refactor or Admin authentication middleware to ES7

--- server/middleware/authenticate-admin.js
+++ server/middleware/authenticate-admin.js
@@ -2,26 +2,27 @@
 
 const User = require('./../models/User');
 
-const authenticate = (req, res , next) => {
-    let token = req.header('x-auth');
-      User.findByToken(token)
-      .then((user) => {
-          if(!user) {
-              return res.sendStatus(401);
-          }
+const authenticate = async (req, res , next) => {
+    const token = req.header('x-auth');
 
-          if(user.isAdmin()){
-              req.user = user;
-              req.token = token;
-              return next(); 
-          }
-          
-          return Promise.reject();
-          
-      })
-      .catch((e) => {
-          return res.sendStatus(401);
-      });
+    try {
+        const user = await User.findByToken(token);
+        if(!user) {
+            return res.sendStatus(401);
+        }
+
+        if(user.isAdmin()){
+            req.user = user;
+            req.token = token;
+            return next(); 
+        }
+
+        throw new Error(`Not authorized`);
+        
+    } catch (error) {
+        return res.sendStatus(401);
+    }
+    
   };
 
 module.exports = {authenticate};
