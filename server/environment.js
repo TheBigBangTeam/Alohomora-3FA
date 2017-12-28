1f944d597a16cfec85e3580c37f1309d7a66e06c
an4cr0n <michelebiondi01@gmail.com>
2017-12-28 16:29:21 +0100

    Refactor of environment node

---
+++ server/environment.js
@@ -0,0 +1,30 @@
+/* ENVIRONMENT */
+const env = process.env.NODE_ENV || 'development';
+
+const setEnvironment = () => {
+    switch(env){
+        case 'development':
+            process.env.PORT = 8080;
+            process.env.MONGODB_URI = 'mongodb://localhost/alohomora-db-devel';
+            break;
+        case 'test':
+            process.env.PORT = 8080;
+            process.env.MONGODB_URI = 'mongodb://localhost/alohomora-db-test';
+            break;
+        case 'production':
+            process.env.PORT = settings.port;
+            process.env.MONGODB_URI = settings.db;
+            process.env.TLS = settings.tls.set;
+            process.env.KEY_PATH = settings.tls.keyPath;
+            process.env.CERT_PATH = settings.tls.certPath;
+            break;
+        default:
+            return console.log('FATAL EXCEPTION: Invalid ENV, be sure to set one of these values: development, test, production.');
+    }
+}
+
+module.exports = {
+    env,
+    setEnvironment 
+    
+};
