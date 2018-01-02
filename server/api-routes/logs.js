4c9b4fdd1b5d847d0d5b103b0e690590db2b2775
an4cr0n <michelebiondi01@gmail.com>
2018-01-02 19:00:24 +0100

    Add log Api route

---
+++ server/api-routes/logs.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require('express');
+const bearerToken = require('express-bearer-token');
+
+const router = express.Router();
+const {authenticate} = require('./../middleware/authenticate-logs');
+const Log = require('./../models/Log');
+
+/* /api/logs API to interact with logs */
+
+router.use(bearerToken());
+router.use(authenticate);
+
+router.get('/', async (req, res) => {
+    const logs = await Log.find();
+    res.json({logs});
+});
---
+++ server/middleware/authenticate-logs.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const User = require('./../models/User');
+const {settings} = require('./../settings');
+
+/* MIDDLEWARE FOR AUTHENTICATION */
+const authenticate = async (req, res , next) => {
+
+    try {
+        const user = await User.findByToken(req.token);
+        if(!user) {
+            return res.sendStatus(401);
+        }
+
+        if(user.isAdmin()){
+            req.user = user;
+            return next(); 
+        }
+
+        if(settings.logPermissionEnum.includes(user.privilege)){
+            req.user = user;
+            return next();
+        }
+
+        throw new Error(`Not authorized`);
+        
+    } catch (error) {
+        return res.sendStatus(401);
+    }
+    
+  };
+
+module.exports = {authenticate};
