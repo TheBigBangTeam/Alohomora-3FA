c9a758a0a9a29965c1abc1aa9c921315bb774177
an4cr0n <michelebiondi01@gmail.com>
2018-01-05 20:36:36 +0100

    Add test for logging functionalities

--- server/src/test/api-routes/authentication.test.js
+++ server/src/test/api-routes/authentication.test.js
@@ -2,11 +2,13 @@
 
 const request = require('supertest')
 const jwt = require('jsonwebtoken')
+const chai = require('chai')
+const expect = chai.expect
 
 const {app} = require('./../../app')
-const {users, devices, populateUsers, populateDevices} = require('./../seed/seed')
+const {users, devices, logs, populateLogs, populateUsers, populateDevices} = require('./../seed/seed')
 const {settings} = require('./../../settings')
-
+const Log = require('./../../models/Log')
 const authenticationPath = '/api/authenticate'
 
 const token = jwt.sign({_id: devices[0]._id.toHexString()}, settings.JWT.secret, {algorithm: settings.JWT.algorithm, issuer: settings.JWT.issuer}).toString()
@@ -16,6 +18,7 @@ const noDbToken = jwt.sign({_id: '5a469a203d4f2410c9cb7632'}, settings.JWT.secre
 describe('[*] AUTHENTICATION ROUTE TEST', () => {
   beforeEach(populateUsers)
   beforeEach(populateDevices)
+  beforeEach(populateLogs)
 
   describe('- GET /:rfid', () => {
     it('should return 200', (done) => {
@@ -23,7 +26,16 @@ describe('[*] AUTHENTICATION ROUTE TEST', () => {
             .get(`${authenticationPath}/${users[0].rfidTag}`)
             .set('Authorization', `Bearer ${token}`)
             .expect(200)
-            .end(done)
+            .end((err, res) => {
+              if (err) return done(err)
+              Log.find({}, (err, logList) => {
+                if (err) {
+                  return done(err)
+                }
+                expect(logList.length).to.equal(logs.length + 1)
+                done()
+              })
+            })
     })
 
     it('should return 401 (invalid token)', (done) => {
@@ -39,7 +51,16 @@ describe('[*] AUTHENTICATION ROUTE TEST', () => {
             .get(`${authenticationPath}/nonexistent`)
             .set('Authorization', `Bearer ${token}`)
             .expect(404)
-            .end(done)
+            .end((err, res) => {
+              if (err) return done(err)
+              Log.find({}, (err, logList) => {
+                if (err) {
+                  return done(err)
+                }
+                expect(logList.length).to.equal(logs.length + 1)
+                done()
+              })
+            })
     })
 
     it('should return 401 (device not found)', (done) => {
@@ -57,7 +78,16 @@ describe('[*] AUTHENTICATION ROUTE TEST', () => {
             .get(`${authenticationPath}/${users[0].rfidTag}/${users[0].pin}`)
             .set('Authorization', `Bearer ${token}`)
             .expect(200)
-            .end(done)
+            .end((err, res) => {
+              if (err) return done(err)
+              Log.find({}, (err, logList) => {
+                if (err) {
+                  return done(err)
+                }
+                expect(logList.length).to.equal(logs.length + 1)
+                done()
+              })
+            })
     })
 
     it('should return 401 ', (done) => {
@@ -65,7 +95,16 @@ describe('[*] AUTHENTICATION ROUTE TEST', () => {
             .get(`${authenticationPath}/${users[0].rfidTag}/28310381028312`)
             .set('Authorization', `Bearer ${token}`)
             .expect(401)
-            .end(done)
+            .end((err, res) => {
+              if (err) return done(err)
+              Log.find({}, (err, logList) => {
+                if (err) {
+                  return done(err)
+                }
+                expect(logList.length).to.equal(logs.length + 1)
+                done()
+              })
+            })
     })
   })
 })
