2b05d1ee0bd8aebc4a1d949852f08ddf53ed62ef
an4cr0n <michelebiondi01@gmail.com>
2018-01-07 23:54:24 +0100

    Improved src folder of react

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <header className='App-header'>
+        <h1 className='App-title'>Welcome to Alohomora Web</h1>
+      </header>
+    </div>
+  </BrowserRouter>
+)
+
+export default App
