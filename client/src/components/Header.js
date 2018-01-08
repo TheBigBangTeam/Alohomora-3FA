d19b95890d28ae95562f830e484bc3c1160cc64b
Daniele Moscati <danielemoscati96@hotmail.it>
2018-01-08 16:36:55 +0100

    Update Header 1

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,11 +4,12 @@ import './styles/header.css'
 const Header = () => (
   <header class="header-login-signup">
   <div class="header-limiter">
-    <h1><a href="#">Company<span>logo</span></a></h1>
+    <h1><a href="#">Alohomora<span>3FA</span></a></h1>
     <nav>
       <a href="#">Home</a>
-      <a href="#">Blog</a>
+      <a href="#">Contacts</a>
       <a href="#">Pricing</a>
+      <a href="#">EULA</a>
     </nav>
     <ul>
       <li><a href="#">Login</a></li>
