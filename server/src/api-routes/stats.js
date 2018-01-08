25c3ead767c0665594a1e8a31943690e90dd70fc
an4cr0n <michelebiondi01@gmail.com>
2018-01-08 17:01:24 +0100

    Add simple stats route

---
+++ server/src/api-routes/stats.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express')
+const bearerToken = require('express-bearer-token')
+
+const router = express.Router()
+const {authenticate} = require('./../middleware/authenticate-stats')
+const Log = require('./../models/Log')
+
+/* /api/logs API to interact with logs */
+
+router.use(bearerToken())
+router.use(authenticate)
+
+// GET all stats
+router.get('/', async (req, res) => {
+  const logs = await Log.find({})
+  console.log(logs)
+  res.json({
+    'total_actions': logs.length,
+    'total_warnings': logs.filter((log) => log.severity === 'warning').length,
+    'total_info': logs.filter((log) => log.severity === 'info').length,
+    'total_fatal': logs.filter((log) => log.severity === 'fatal').length
+  })
+})
+
+module.exports = router
