fb54bcefe220b302e6effc541329ffa3527bea97
Daniele Moscati <danielemoscati96@hotmail.it>
2018-01-08 16:31:10 +0100

    Base Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './styles/header.css'
 
 const Header = () => (
-  <header>
-    <h1>Welcome to Alohomora Web</h1>
+  <header class="header-login-signup">
+  <div class="header-limiter">
+    <h1><a href="#">Company<span>logo</span></a></h1>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Blog</a>
+      <a href="#">Pricing</a>
+    </nav>
+    <ul>
+      <li><a href="#">Login</a></li>
+      <li><a href="#">Sign up</a></li>
+    </ul>
+  </div>
   </header>
 )
 
