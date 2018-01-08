5fb5ce6fc109dac6fb0934237889cb40a276b9e6
Daniele Moscati <danielemoscati96@hotmail.it>
2018-01-08 18:03:08 +0100

    Removed MaterialUI & Update header 2

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-
 import Header from './Header'
 import Homepage from './Homepage'
 import NotFound from './NotFound'
