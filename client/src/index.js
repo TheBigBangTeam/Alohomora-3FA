f1493b2cdeab77c17b3a744b8c6177126f17dc16
an4cr0n <michelebiondi01@gmail.com>
2018-01-09 16:37:46 +0100

    Redux set up

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import rootReducer from './rootReducer'
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
 
