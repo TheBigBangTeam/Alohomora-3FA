e2d95d1c6dfc41dfd8dd4cc8e0b24be4cec20e56
Daniele Moscati <danielemoscati96@hotmail.it>
2018-01-17 23:04:01 +0100

    Updated all the headers with associated buttons

--- client/src/components/header/HeaderButtonsLogs.js
+++ client/src/components/header/HeaderButtonsLogs.js
@@ -1,36 +1,13 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
-import IconButton from 'material-ui/IconButton'
-import IconMenu from 'material-ui/IconMenu'
-import MenuItem from 'material-ui/MenuItem'
-import LogsBox from 'material-ui/svg-icons/action/pageview'
-import {white} from 'material-ui/styles/colors'
+import LogsButton from './LogsButton'
 
 class HeaderButtonslogs extends Component {
 
     render() {
-        const {styles} = this.props
-        const style = {
-            buttonsContainer: {
-              marginLeft: 20,
-            },
-            link: {
-                textDecoration: 'none',
-                color: '#FFF'
-            }
-        }
         return(
             <a>
-                <IconMenu color={white} style={{...styles, ...style.buttonsContainer}}
-                    iconButtonElement={
-                    <IconButton><LogsBox color={white}/></IconButton>
-                    }
-                    targetOrigin={{horizontal: 'left', vertical: 'top'}}
-                    anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-                >
-                    <Link to='/logs' style={style.link}><MenuItem key={1} primaryText="View Logs"/></Link>
-                </IconMenu>
+                <LogsButton />
             </a>
         )
     }
