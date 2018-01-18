7a00dc1566a70dbd5321da27cffa162171b29efd
Daniele Moscati <danielemoscati96@hotmail.it>
2018-01-18 12:20:10 +0100

    Fixed bad request issue

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default {
   user: {
     login: (credentials) => axios.post('/api/user', credentials).then(res => res.data),
-    insertUser: (data) => axios.post('/api/admin', data, {
+    insertUser: (data) => axios.post('/api/admin/users', data, {
       'headers': {
         'Authorization' : 'Bearer ' + localStorage.alohomoraToken
       }
