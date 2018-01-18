f89282bb3bab242082cd88f86fa893b45dd610b3
Daniele Moscati <danielemoscati96@hotmail.it>
2018-01-18 11:58:11 +0100

    Fixed unauthorized access issue

--- client/src/api.js
+++ client/src/api.js
@@ -3,6 +3,10 @@ import axios from 'axios'
 export default {
   user: {
     login: (credentials) => axios.post('/api/user', credentials).then(res => res.data),
-    insertUser: (data) => axios.post('/api/admin', data).then(res => res.data),
+    insertUser: (data) => axios.post('/api/admin', data, {
+      'headers': {
+        'Authorization' : 'Bearer ' + localStorage.alohomoraToken
+      }
+    }).then(res => res.data),
   }
 }
