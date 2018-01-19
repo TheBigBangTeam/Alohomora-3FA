c1a9149d955a35baaa926a2abd1d24953b690067
Michele Biondi <michelebiondi01@gmail.com>
2018-01-19 19:01:30 +0100

    Add mailer to utilities

--- server/src/utilities.js
+++ server/src/utilities.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const crypto = require('crypto')
+const nodemailer = require('nodemailer')
 
 const encryptAES = (length, mode, secret, plaintext) => {
   let cipher
@@ -31,7 +32,42 @@ const decryptAES = (length, mode, secret, ciphertext) => {
   return decrypted
 }
 
+let mailConfig
+let transporter
+if (process.env.NODE_ENV === 'production' ){
+    // all emails are delivered to destination
+    mailConfig = {
+        host: '',
+        port: 587,
+        auth: {
+            user: '',
+            user: ''
+        }
+    };
+    transporter = nodemailer.createTransport(mailConfig);
+} else {
+    // all emails are catched by ethereal.email
+    nodemailer.createTestAccount((err, account) => {
+      mailConfig = {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: account.user, // generated ethereal user
+            pass: account.pass  // generated ethereal password
+        }
+      }
+      transporter = nodemailer.createTransport(mailConfig);
+      console.log(transporter)
+    });
+    
+}
+
+
+
+
 module.exports = {
   encryptAES,
-  decryptAES
+  decryptAES,
+  transporter
 }
