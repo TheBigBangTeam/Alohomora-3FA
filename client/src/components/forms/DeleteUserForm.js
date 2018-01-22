02c8d7ee9a1b9fc5471f8af355b3db273570d530
Daniele Moscati <danielemoscati96@hotmail.it>
2018-01-22 16:40:15 +0100

    Delete User Page & Form

---
+++ client/src/components/forms/DeleteUserForm.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+class DeleteUserForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      data: {},
+      errors: {}
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (e) {
+    let value
+    this.setState({
+      data: {...this.state.data, [e.target.name]: value}
+    })
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+    const errors = this.validate(this.state.data)
+    this.setState({ errors })
+    if (Object.keys(errors).length === 0) {
+      this.props.submit(this.state.data)
+    }
+  }
+
+  validate (data) {
+    const errors = {}
+    if (!data.id) errors.username = 'Missing ID'
+    return errors
+  }
+
+  render () {
+    return (
+      <MuiThemeProvider>
+        <div style={styles.general}>
+          <form>
+            <TextField
+              style={styles.textField}
+              floatingLabelText='Username'
+              errorText={this.state.errors.username}
+              name='id'
+              type='text'
+              hintText='Enter the ID'
+              onChange={this.handleChange} />
+            <br />
+            <RaisedButton
+              primary
+              style={styles.submitButton}
+              type='submit'
+              className='submitButton'
+              disabled={!(!!this.state.data.id)}
+              label='Delete'
+              onClick={this.handleSubmit} />
+          </form>
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+const styles = {
+  general: {
+    marginTop: '60px',
+    textAlign: 'center'
+  },
+  submitButton: {
+    margin: '15px'
+  },
+  textField: {
+    width: '400px'
+  }
+}
+
+DeleteUserForm.propTypes = {
+  submit: PropTypes.func.isRequired
+}
+
+export default DeleteUserForm
--- client/src/components/pages/DeleteUserPage.js
+++ client/src/components/pages/DeleteUserPage.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import DeleteUserForm from '../forms/DeleteUserForm'
+import {deleteUser} from '../../actions/deletion'
 
 class DeleteUserPage extends Component {
+    constructor (props) {
+        super(props)
+        this.submit = this.submit.bind(this)
+    }
+
+    submit (data) {
+        this.props.deleteUser(data).then(() => this.props.history.push('/dashboard'))
+    }
 
     render() {
         return(
             <div>
-                <h1>DeleteUser Page</h1>
+                <DeleteUserForm submit={this.submit} />
             </div>
         )
     }
-
 }
 
-export default DeleteUserPage
+DeleteUserPage.propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired
+    }).isRequired,
+    deleteUser: PropTypes.func.isRequired
+}
+
+export default connect(null, {deleteUser})(DeleteUserPage)
