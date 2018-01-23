ce58f0ca2df4ed873727fb0773a0be949cb10842
Daniele Moscati <danielemoscati96@hotmail.it>
2018-01-23 17:43:14 +0100

    Connected list items to the action

--- client/src/components/pages/DeleteUserPage.js
+++ client/src/components/pages/DeleteUserPage.js
@@ -1,17 +1,21 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
 import axios from 'axios'
 import {List, ListItem} from 'material-ui/List'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import ActionGrade from 'material-ui/svg-icons/action/grade'
+import RaisedButton from 'material-ui/RaisedButton'
 import Avatar from 'material-ui/Avatar'
-import {pinkA200, transparent} from 'material-ui/styles/colors'
+import {deleteUser} from '../../actions/deletion'
 
 class DeleteUserPage extends Component {
     constructor (props) {
         super(props)
         this.state = {
-            users: []
+            users: [],
+            data: {}
         }
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount(){
@@ -30,20 +34,47 @@ class DeleteUserPage extends Component {
         })
     }
 
+    handleSubmit (e) {
+        e.preventDefault()
+        this.props.deleteUser(this.state.data).then(() => this.props.history.push('/dashboard'))
+    }
+
     render() {
         return(
             <MuiThemeProvider>
                 <List>
                 {this.state.users.map(user =>
                 <ListItem
+                    key={user._id}
                     primaryText={user.name + " " + user.surname}
                     rightAvatar={<Avatar>{user.name[0]}</Avatar>}
+                    onClick={() => {
+                        var id2 = user._id.toString()
+                        this.setState({
+                            data: {...this.state.data, id: id2}
+                        })
+                    }}
                 />
                 )}
                 </List>
+                <RaisedButton
+                    primary
+                    type='submit'
+                    className='submitButton'
+                    disabled={!this.state.data.id}
+                    label='Delete'
+                    onClick={this.handleSubmit} 
+                />
             </MuiThemeProvider>
         )
     }
 }
 
-export default DeleteUserPage
+DeleteUserPage.propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired
+    }).isRequired,
+    deleteUser: PropTypes.func.isRequired
+  }
+
+export default connect(null, {deleteUser})(DeleteUserPage)
