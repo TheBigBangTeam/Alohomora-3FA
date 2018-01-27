ee2c046f7f6847b3a4c0f99eac9f4ac832b43e07
Daniele Moscati <danielemoscati96@hotmail.it>
2018-01-27 12:42:40 +0100

    Restyling

--- client/src/components/pages/LogsPage.js
+++ client/src/components/pages/LogsPage.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import axios from 'axios'
 import {Card, CardMedia} from 'material-ui/Card'
+import Divider from 'material-ui/Divider'
+import Paper from 'material-ui/Paper'
+import TextField from 'material-ui/TextField'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class Logs extends Component {
@@ -31,45 +34,148 @@ class Logs extends Component {
         return(
             <MuiThemeProvider>
                 <div>
+                    <font face="Roboto">
+                        <h2>Logs Page</h2>
+                    </font>
                     <Card>
                         <CardMedia>
                             {this.state.logs.map(log =>
                             log.severity==='info' ?
                                 <div>
+                                    <Paper zDepth={2}>
                                     <font color="blue"><h2>{log.severity}</h2></font>
                                     <p style={{paddingLeft: 10}}>
-                                        {log._id ? <font>ID: {log._id}<br /></font> : null}
-                                        {log.user ? <font>User: {log.user}<br /></font> : null}
-                                        {log.device ? <font>Device: {log.device}<br /></font> : null}
-                                        {log.description ? <font>Description: {log.description}</font>: null}
+                                        {log._id ? 
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"ID: "+log._id} 
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
+                                        {log.user ?
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"User: "+log.user}
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
+                                        {log.device ?
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"Device: "+log.device}
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
+                                        {log.description ?
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"Description: "+log.description} 
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
                                     </p>
+                                    </Paper>
                                 </div>
                             :
                             log.severity==='warning' ?
                                 <div>
+                                    <Paper>
                                     <font color="orange"><h2>{log.severity}</h2></font>
                                     <p style={{paddingLeft: 10}}>
-                                        {log._id ? <font>ID: {log._id}<br /></font> : null}
-                                        {log.user ? <font>User: {log.user}<br /></font> : null}
-                                        {log.device ? <font>Device: {log.device}<br /></font> : null}
-                                        {log.description ? <font>Description: {log.description}</font>: null}
+                                        {log._id ? 
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"ID: "+log._id} 
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
+                                        {log.user ?
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"User: "+log.user}
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
+                                        {log.device ?
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"Device: "+log.device}
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
+                                        {log.description ?
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"Description: "+log.description} 
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
                                     </p>
+                                    </Paper>
                                 </div>
                             :
                             log.severity==='fatal' ?
                                 <div>
+                                    <Paper>
                                     <font color="red"><h1>{log.severity}</h1></font>
                                     <p style={{paddingLeft: 10}}>
-                                        {log._id ? <font>ID: {log._id}<br /></font> : null}
-                                        {log.user ? <font>User: {log.user}<br /></font> : null}
-                                        {log.device ? <font>Device: {log.device}<br /></font> : null}
-                                        {log.description ? <font>Description: {log.description}</font>: null}
+                                        {log._id ? 
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"ID: "+log._id} 
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
+                                        {log.user ?
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"User: "+log.user}
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
+                                        {log.device ?
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"Device: "+log.device}
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
+                                        {log.description ?
+                                        <TextField
+                                            disabled={false}
+                                            underlineShow={false}
+                                            defaultValue={"Description: "+log.description} 
+                                            fullWidth={true}
+                                        /> : null
+                                        }
+                                        <Divider />
                                     </p>
+                                    </Paper>
                                 </div>
                             :
-                                <div>
-                                    <font>{log.severity}</font>
-                                </div>
+                            null
                             )}
                         </CardMedia>
                     </Card>
