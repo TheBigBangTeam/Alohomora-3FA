c3440fcd5aa42c54fca4578cf62c863d727a6b5a
Daniele Moscati <danielemoscati96@hotmail.it>
2018-01-27 10:50:19 +0100

    JSON receive action and iteration

--- client/src/components/pages/LogsPage.js
+++ client/src/components/pages/LogsPage.js
@@ -1,6 +1,29 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 class Logs extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            logs: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('/api/logs', {
+            'headers': {
+                'Authorization' : 'Bearer ' + localStorage.alohomoraToken
+              }
+        }).then(({data}) => {
+            var array = []
+            for(var i = 0; i<data.logs.length; i++) {
+                array.push(data.logs[i])
+            }
+            this.setState({
+                logs: array
+            })
+        })
+    }
 
     render() {
         return(
