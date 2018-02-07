2debb224f2a19ad038c6c380a4eea8986d733fb1
Rygar Nelson <danielemoscati96@hotmail.it>
2018-02-07 21:04:40 +0100

    Removed logout action from "Dashboard"

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,31 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {Card, CardActions, CardHeader} from 'material-ui/Card'
 import PropTypes from 'prop-types'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import FlatButton from 'material-ui/FlatButton'
-import * as actions from '../../actions/auth'
 
-const Dashboard = ({logout}) => (
-  <div>
-    <MuiThemeProvider>
-      <Card>
-        <CardHeader
-          titleColor='red'
-          titleStyle={{fontSize: '40px'}}
-          title='Dashboard'
-        />
-        <CardActions>
-          <FlatButton onClick={() => logout()} label='Logout' /> 
-        </CardActions>
-      </Card>
-    </MuiThemeProvider>
-  </div>
-)
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <Card>
+            <CardHeader
+              titleColor='red'
+              titleStyle={{ fontSize: '40px' }}
+              title='Dashboard'
+            />
+            <CardActions>
+              <h3>
+                <font face="Roboto">
+                  Welcome
+                </font>
+              </h3>
+            </CardActions>
+          </Card>
+        </MuiThemeProvider>
+      </div>
+      )
+  }
+}
 
 Dashboard.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
-  logout: PropTypes.func.isRequired
 }
 
 function mapStateToProps (state) {
@@ -34,4 +39,4 @@ function mapStateToProps (state) {
   }
 }
 
-export default connect(mapStateToProps, {logout: actions.logout})(Dashboard)
+export default connect(mapStateToProps)(Dashboard)
