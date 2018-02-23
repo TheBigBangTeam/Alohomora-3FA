c71d1224882cd3473ec49a07c1442bba4c70ebf6
Daniele Moscati <danielemoscati96@hotmail.it>
2018-02-23 15:26:25 +0100

    Modified Dashboard. Now it shows the last log and an example of statistic

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import {Card, CardActions, CardHeader} from 'material-ui/Card'
+import axios from 'axios'
+import { Card, CardMedia, CardHeader } from 'material-ui/Card'
 import PropTypes from 'prop-types'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { LineChart } from 'react-chartkick'
+import Paper from 'material-ui/Paper'
+import LogsInfo from './LogsInfo'
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      log: [],
+      stats: {}
+    }
+  }
+
+  componentDidMount() {
+    //I will take the stats
+    axios.get('/api/stats', {
+      'headers': {
+        'Authorization': 'Bearer ' + localStorage.alohomoraToken
+      }
+    }).then(({ data }) => {
+      var array = JSON.stringify(data)
+      this.setState({
+        stats: JSON.parse(array)
+      })
+    })
+    //I will take the last log
+    axios.get('/api/logs', {
+      'headers': {
+        'Authorization': 'Bearer ' + localStorage.alohomoraToken
+      }
+    }).then(({ data }) => {
+      var array = []
+      var lastLog = data.logs.length - 1
+      if (lastLog) {
+        array.push(lastLog)
+      }
+      this.setState({
+        log: array
+      })
+    })
+  }
+
   render() {
+    const name = this.props.name
+    const surname = this.props.surname
+
     return (
       <div>
         <MuiThemeProvider>
@@ -15,27 +59,90 @@ class Dashboard extends Component {
               titleStyle={{ fontSize: '40px' }}
               title='Dashboard'
             />
-            <CardActions>
+            <CardMedia>
               <h3>
                 <font face="Roboto">
-                  Welcome
+                  Welcome back {name + " " + surname}
                 </font>
               </h3>
-            </CardActions>
+            </CardMedia>
+            <CardMedia>
+              <font face="Roboto">
+                <h2>Last Log</h2>
+              </font>
+              {this.state.log.map(log =>
+                log.severity === 'info' ?
+                  <div>
+                    <Paper zDepth={2}>
+                      <font color="blue"><h2>{log.severity}</h2></font>
+                      <p style={{ paddingLeft: 10 }}>
+                        <LogsInfo
+                          id={log._id}
+                          user={log.user}
+                          device={log.device}
+                          description={log.description}
+                        />
+                      </p>
+                    </Paper>
+                  </div>
+                  :
+                  log.severity === 'warning' ?
+                    <div>
+                      <Paper>
+                        <font color="orange"><h2>{log.severity}</h2></font>
+                        <p style={{ paddingLeft: 10 }}>
+                          <LogsInfo
+                            id={log._id}
+                            user={log.user}
+                            device={log.device}
+                            description={log.description}
+                          />
+                        </p>
+                      </Paper>
+                    </div>
+                    :
+                    log.severity === 'fatal' ?
+                      <div>
+                        <Paper>
+                          <font color="red"><h1>{log.severity}</h1></font>
+                          <p style={{ paddingLeft: 10 }}>
+                            <LogsInfo
+                              id={log._id}
+                              user={log.user}
+                              device={log.device}
+                              description={log.description}
+                            />
+                          </p>
+                        </Paper>
+                      </div>
+                      :
+                      null
+              )}
+            </CardMedia>
+            <CardMedia>
+              <font face="Roboto">
+                <h2>Last Statistic</h2>
+              </font>
+              <LineChart
+                data={this.state.stats}
+              />
+            </CardMedia>
           </Card>
         </MuiThemeProvider>
       </div>
-      )
+    )
   }
 }
 
 Dashboard.propTypes = {
-  isAuthenticated: PropTypes.bool.isRequired,
+  name: PropTypes.string,
+  surname: PropTypes.string
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
-    isAuthenticated: !!state.user.token
+    name: state.user.user.name,
+    surname: state.user.user.surname
   }
 }
 
